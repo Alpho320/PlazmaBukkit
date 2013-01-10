1056b8eb909c157d1590a603e93e759a0193f850
Aikar <aikar@aikar.co>
2013-01-10 00:18:11 -0500

    Spigot Timings
    Overhauls the Timings System adding performance tracking all around the Minecraft Server

--- src/main/java/net/minecraft/server/network/PlayerConnection.java
+++ src/main/java/net/minecraft/server/network/PlayerConnection.java
@@ -339,6 +339,7 @@ public class PlayerConnection implements ServerPlayerConnection, TickablePacketL
 
     @Override
     public void tick() {
+        org.bukkit.craftbukkit.SpigotTimings.playerConnectionTimer.startTiming(); // Spigot
         if (this.ackBlockChangesUpTo > -1) {
             this.send(new ClientboundBlockChangedAckPacket(this.ackBlockChangesUpTo));
             this.ackBlockChangesUpTo = -1;
@@ -419,6 +420,7 @@ public class PlayerConnection implements ServerPlayerConnection, TickablePacketL
             this.player.resetLastActionTime(); // CraftBukkit - SPIGOT-854
             this.disconnect(IChatBaseComponent.translatable("multiplayer.disconnect.idling"));
         }
+        org.bukkit.craftbukkit.SpigotTimings.playerConnectionTimer.stopTiming(); // Spigot
 
     }
 
@@ -2100,6 +2102,7 @@ public class PlayerConnection implements ServerPlayerConnection, TickablePacketL
     }
 
     private void handleCommand(String s) {
+        org.bukkit.craftbukkit.SpigotTimings.playerCommandTimer.startTiming(); // Spigot
         this.LOGGER.info(this.player.getScoreboardName() + " issued server command: " + s);
 
         CraftPlayer player = this.getCraftPlayer();
@@ -2108,6 +2111,7 @@ public class PlayerConnection implements ServerPlayerConnection, TickablePacketL
         this.cserver.getPluginManager().callEvent(event);
 
         if (event.isCancelled()) {
+            org.bukkit.craftbukkit.SpigotTimings.playerCommandTimer.stopTiming(); // Spigot
             return;
         }
 
@@ -2119,6 +2123,8 @@ public class PlayerConnection implements ServerPlayerConnection, TickablePacketL
             player.sendMessage(org.bukkit.ChatColor.RED + "An internal error occurred while attempting to perform this command");
             java.util.logging.Logger.getLogger(PlayerConnection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
             return;
+        } finally {
+            org.bukkit.craftbukkit.SpigotTimings.playerCommandTimer.stopTiming(); // Spigot
         }
     }
     // CraftBukkit end
