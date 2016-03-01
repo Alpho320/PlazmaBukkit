e469efc03270d36773f303100abb687c7ab96072
Zach Brown <zach.brown@destroystokyo.com>
2016-03-01 13:14:11 -0600

    Configurable fishing time ranges

--- src/main/java/net/minecraft/world/entity/projectile/FishingHook.java
+++ src/main/java/net/minecraft/world/entity/projectile/FishingHook.java
@@ -92,6 +92,10 @@ public class FishingHook extends Projectile {
         this.noCulling = true;
         this.luck = Math.max(0, luckOfTheSeaLevel);
         this.lureSpeed = Math.max(0, lureLevel);
+        // Paper start
+        minWaitTime = world.paperConfig().fishingTimeRange.minimum;
+        maxWaitTime = world.paperConfig().fishingTimeRange.maximum;
+        // Paper end
     }
 
     public FishingHook(EntityType<? extends FishingHook> type, Level world) {
@@ -409,7 +413,7 @@ public class FishingHook extends Projectile {
             } else {
                 // CraftBukkit start - logic to modify fishing wait time
                 this.timeUntilLured = Mth.nextInt(this.random, this.minWaitTime, this.maxWaitTime);
-                this.timeUntilLured -= (this.applyLure) ? this.lureSpeed * 20 * 5 : 0;
+                this.timeUntilLured -= (this.applyLure) ? (this.lureSpeed * 20 * 5 >= this.maxWaitTime ? this.timeUntilLured - 1 : this.lureSpeed * 20 * 5) : 0; // Paper - Fix Lure infinite loop
                 // CraftBukkit end
             }
         }
