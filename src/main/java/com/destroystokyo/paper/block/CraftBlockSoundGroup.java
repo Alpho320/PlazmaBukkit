PaperMC/Paper

Implement CraftBlockSoundGroup

paper-server/src/main/java/com/destroystokyo/paper/block/CraftBlockSoundGroup.java
@@ -1,0 +1,39 @@
+package com.destroystokyo.paper.block;
+
+import net.minecraft.world.level.block.SoundType;
+import org.bukkit.Sound;
+import org.bukkit.craftbukkit.CraftSound;
+
+@Deprecated(forRemoval = true)
+public class CraftBlockSoundGroup implements BlockSoundGroup {
+    private final SoundType soundEffectType;
+
+    public CraftBlockSoundGroup(SoundType soundEffectType) {
+        this.soundEffectType = soundEffectType;
+    }
+
+    @Override
+    public Sound getBreakSound() {
+        return CraftSound.minecraftToBukkit(soundEffectType.getBreakSound());
+    }
+
+    @Override
+    public Sound getStepSound() {
+        return CraftSound.minecraftToBukkit(soundEffectType.getStepSound());
+    }
+
+    @Override
+    public Sound getPlaceSound() {
+        return CraftSound.minecraftToBukkit(soundEffectType.getPlaceSound());
+    }
+
+    @Override
+    public Sound getHitSound() {
+        return CraftSound.minecraftToBukkit(soundEffectType.getHitSound());
+    }
+
+    @Override
+    public Sound getFallSound() {
+        return CraftSound.minecraftToBukkit(soundEffectType.getFallSound());
+    }
+}

Alpho320/PlazmaBukkit

src/main/java/com/destroystokyo/paper/block/CraftBlockSoundGroup.java
@@ -1,0 +1,39 @@
+package com.destroystokyo.paper.block;
+
+import net.minecraft.world.level.block.SoundType;
+import org.bukkit.Sound;
+import org.bukkit.craftbukkit.CraftSound;
+
+@Deprecated(forRemoval = true)
+public class CraftBlockSoundGroup implements BlockSoundGroup {
+    private final SoundType soundEffectType;
+
+    public CraftBlockSoundGroup(SoundType soundEffectType) {
+        this.soundEffectType = soundEffectType;
+    }
+
+    @Override
+    public Sound getBreakSound() {
+        return CraftSound.getBukkit(soundEffectType.getBreakSound());
+    }
+
+    @Override
+    public Sound getStepSound() {
+        return CraftSound.getBukkit(soundEffectType.getStepSound());
+    }
+
+    @Override
+    public Sound getPlaceSound() {
+        return CraftSound.getBukkit(soundEffectType.getPlaceSound());
+    }
+
+    @Override
+    public Sound getHitSound() {
+        return CraftSound.getBukkit(soundEffectType.getHitSound());
+    }
+
+    @Override
+    public Sound getFallSound() {
+        return CraftSound.getBukkit(soundEffectType.getFallSound());
+    }
+}