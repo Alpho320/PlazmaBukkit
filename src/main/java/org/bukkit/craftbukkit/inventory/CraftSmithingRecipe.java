Machine-Maker/PaperHistoryTest

Option to prevent NBT copy in smithing recipes

server/src/main/java/org/bukkit/craftbukkit/inventory/CraftSmithingRecipe.java
@@ -8,15 +8,21 @@ import org.bukkit.inventory.RecipeChoice;
 import org.bukkit.inventory.SmithingRecipe;
 
 public class CraftSmithingRecipe extends SmithingRecipe implements CraftRecipe {
+    @Deprecated // Paper
     public CraftSmithingRecipe(NamespacedKey key, ItemStack result, RecipeChoice base, RecipeChoice addition) {
         super(key, result, base, addition);
     }
+    // Paper start
+    public CraftSmithingRecipe(NamespacedKey key, ItemStack result, RecipeChoice base, RecipeChoice addition, boolean copyNbt) {
+        super(key, result, base, addition, copyNbt);
+    }
+    // Paper end
 
     public static CraftSmithingRecipe fromBukkitRecipe(SmithingRecipe recipe) {
         if (recipe instanceof CraftSmithingRecipe) {
             return (CraftSmithingRecipe) recipe;
         }
-        CraftSmithingRecipe ret = new CraftSmithingRecipe(recipe.getKey(), recipe.getResult(), recipe.getBase(), recipe.getAddition());
+        CraftSmithingRecipe ret = new CraftSmithingRecipe(recipe.getKey(), recipe.getResult(), recipe.getBase(), recipe.getAddition(), recipe.willCopyNbt()); // Paper
         return ret;
     }
 
@@ -24,6 +30,6 @@ public class CraftSmithingRecipe extends SmithingRecipe implements CraftRecipe {
     public void addToCraftingManager() {
         ItemStack result = this.getResult();
 
-        MinecraftServer.getServer().getRecipeManager().addRecipe(new net.minecraft.world.item.crafting.UpgradeRecipe(CraftNamespacedKey.toMinecraft(this.getKey()), toNMS(this.getBase(), true), toNMS(this.getAddition(), true), CraftItemStack.asNMSCopy(result)));
+        MinecraftServer.getServer().getRecipeManager().addRecipe(new net.minecraft.world.item.crafting.UpgradeRecipe(CraftNamespacedKey.toMinecraft(this.getKey()), toNMS(this.getBase(), true), toNMS(this.getAddition(), true), CraftItemStack.asNMSCopy(result), this.willCopyNbt())); // Paper
     }
 }

Alpho320/PlazmaBukkit

src/main/java/org/bukkit/craftbukkit/inventory/CraftSmithingRecipe.java
@@ -8,15 +8,21 @@ import org.bukkit.inventory.RecipeChoice;
 import org.bukkit.inventory.SmithingRecipe;
 
 public class CraftSmithingRecipe extends SmithingRecipe implements CraftRecipe {
+    @Deprecated // Paper
     public CraftSmithingRecipe(NamespacedKey key, ItemStack result, RecipeChoice base, RecipeChoice addition) {
         super(key, result, base, addition);
     }
+    // Paper start
+    public CraftSmithingRecipe(NamespacedKey key, ItemStack result, RecipeChoice base, RecipeChoice addition, boolean copyNbt) {
+        super(key, result, base, addition, copyNbt);
+    }
+    // Paper end
 
     public static CraftSmithingRecipe fromBukkitRecipe(SmithingRecipe recipe) {
         if (recipe instanceof CraftSmithingRecipe) {
             return (CraftSmithingRecipe) recipe;
         }
-        CraftSmithingRecipe ret = new CraftSmithingRecipe(recipe.getKey(), recipe.getResult(), recipe.getBase(), recipe.getAddition());
+        CraftSmithingRecipe ret = new CraftSmithingRecipe(recipe.getKey(), recipe.getResult(), recipe.getBase(), recipe.getAddition(), recipe.willCopyNbt()); // Paper
         return ret;
     }
 
@@ -24,6 +30,6 @@ public class CraftSmithingRecipe extends SmithingRecipe implements CraftRecipe {
     public void addToCraftingManager() {
         ItemStack result = this.getResult();
 
-        MinecraftServer.getServer().getRecipeManager().addRecipe(new net.minecraft.world.item.crafting.LegacyUpgradeRecipe(CraftNamespacedKey.toMinecraft(this.getKey()), toNMS(this.getBase(), true), toNMS(this.getAddition(), true), CraftItemStack.asNMSCopy(result)));
+        MinecraftServer.getServer().getRecipeManager().addRecipe(new net.minecraft.world.item.crafting.LegacyUpgradeRecipe(CraftNamespacedKey.toMinecraft(this.getKey()), toNMS(this.getBase(), true), toNMS(this.getAddition(), true), CraftItemStack.asNMSCopy(result), this.willCopyNbt())); // Paper
     }
 }